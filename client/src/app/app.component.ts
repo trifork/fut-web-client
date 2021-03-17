000ad04c3cb2e9fea9974b52aaa67704bf4f2662
ttn <ttn@trifork.com>
2021-03-18 00:03:25 +0100

    Show package.json version in bottom right corner

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from './user/user.service';
+import { version } from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,7 @@ import {UserService} from './user/user.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  public version: string = version;
   title = 'client';
   showSidenav = true;
 
